49f9fa372cf1d95f77a249c8610806f1c8db8047
Ciro Cattuto <ciro.cattuto@gmail.com>
2024-04-26 21:44:57 +0200

    fix state machine literals

--- uart/Uart8Receiver.v
+++ uart/Uart8Receiver.v
@@ -17,13 +17,7 @@ module Uart8Receiver (
     output reg        busy, // transaction is in process
     output reg        err   // error while receiving data
 );
-    // states of state machine
-    reg [1:0] RESET = 2'b00;
-    reg [1:0] IDLE = 2'b01;
-    reg [1:0] DATA_BITS = 2'b10;
-    reg [1:0] STOP_BIT = 2'b11;
-
-    reg [2:0] state;
+    reg [2:0] state = `RESET;
     reg [2:0] bitIdx = 3'b0; // for 8-bit data
     reg [1:0] inputSw = 2'b0; // shift reg for input signal state
     reg [3:0] clockCount = 4'b0; // count clocks for 16x oversample
@@ -40,11 +34,11 @@ module Uart8Receiver (
         inputSw <= { inputSw[0], in };
 
         if (!en) begin
-            state <= RESET;
+            state <= `RESET;
         end
 
         case (state)
-            RESET: begin
+            `RESET: begin
                 out <= 8'b0;
                 err <= 1'b0;
                 done <= 1'b0;
@@ -53,14 +47,14 @@ module Uart8Receiver (
                 clockCount <= 4'b0;
                 receivedData <= 8'b0;
                 if (en) begin
-                    state <= IDLE;
+                    state <= `IDLE;
                 end
             end
 
-            IDLE: begin
+            `IDLE: begin
                 done <= 1'b0;
                 if (clockCount >= 4'h8) begin
-                    state <= DATA_BITS;
+                    state <= `DATA_BITS;
                     out <= 8'b0;
                     bitIdx <= 3'b0;
                     clockCount <= 4'b0;
@@ -71,21 +65,21 @@ module Uart8Receiver (
                     // Check bit to make sure it's still low
                     if (&inputSw) begin
                         err <= 1'b1;
-                        state <= RESET;
+                        state <= `RESET;
                     end
                     clockCount <= clockCount + 4'b1;
                 end
             end
 
             // Wait 8 full cycles to receive serial data
-            DATA_BITS: begin
+            `DATA_BITS: begin
                 if (&clockCount) begin // save one bit of received data
                     clockCount <= 4'b0;
                     // TODO: check the most popular value
                     receivedData[bitIdx] <= inputSw[0];
                     if (&bitIdx) begin
                         bitIdx <= 3'b0;
-                        state <= STOP_BIT;
+                        state <= `STOP_BIT;
                     end else begin
                         bitIdx <= bitIdx + 3'b1;
                     end
@@ -98,9 +92,9 @@ module Uart8Receiver (
             * Baud clock may not be running at exactly the same rate as the
             * transmitter. Next start bit is allowed on at least half of stop bit.
             */
-            STOP_BIT: begin
+            `STOP_BIT: begin
                 if (&clockCount || (clockCount >= 4'h8 && !(|inputSw))) begin
-                    state <= IDLE;
+                    state <= `IDLE;
                     done <= 1'b1;
                     busy <= 1'b0;
                     out <= receivedData;
@@ -110,12 +104,12 @@ module Uart8Receiver (
                     // Check bit to make sure it's still high
                     if (!(|inputSw)) begin
                         err <= 1'b1;
-                        state <= RESET;
+                        state <= `RESET;
                     end
                 end
             end
 
-            default: state <= IDLE;
+            default: state <= `IDLE;
         endcase
     end
 
