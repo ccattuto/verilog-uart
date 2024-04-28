7d136041f877ff66a2c8db74552ccf8ddd91a291
Ciro Cattuto <ciro.cattuto@gmail.com>
2024-04-28 15:06:36 +0200

    file headers

--- uart/Uart8Receiver.v
+++ uart/Uart8Receiver.v
@@ -1,10 +1,33 @@
 //
-// UART Receiver
+// Simple Verilog implementation of an UART receiver
 //
-// Able to receive 8 bits of serial data, one start bit, one stop bit.
-// When receive is complete {valid} is driven high for one clock cycle.
-// Output data should be taken away by a few clocks or can be lost.
-// Clock should be decreased to baud rate.
+//  - 8N1 only (8-bit data word, 1 start bit, 1 stop bit)
+//  - "valid" goes high for exactly 1 clock cycle after successful RX 
+//
+//
+// MIT License
+//
+// Copyright (c) 2024 Ciro Cattuto <ciro.cattuto@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// Originally based on Dmitry Pchelkin's verilog-uart (https://github.com/hell03end/verilog-uart)
 //
 
  // states of state machine
--- uart/Uart8Transmitter.v
+++ uart/Uart8Transmitter.v
@@ -1,7 +1,34 @@
 //
-// UART Transmitter
-// 
+// Simple Verilog implementation of an UART transmitter
 //
+//  - 8N1 only (8-bit data word, 1 start bit, 1 stop bit)
+//  - "ready" is high when ready for TX
+//  - user drives "valid" high for 1 clock cycle to start TX 
+//
+//
+// MIT License
+//
+// Copyright (c) 2024 Ciro Cattuto <ciro.cattuto@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// Originally based on Dmitry Pchelkin's verilog-uart (https://github.com/hell03end/verilog-uart)
 //
 
  // states of state machine
